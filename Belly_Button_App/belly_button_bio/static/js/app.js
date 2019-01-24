c235aed24da6dbe65ef08c791dbb201a639b6d1d
kreitlerj <kreitlerj11@yahoo.com>
2019-01-24 09:34:41 -0600

    Final updates, correction to the requirements.txt to fix app crash.

--- Belly_Button_App/belly_button_bio/static/js/app.js
+++ Belly_Button_App/belly_button_bio/static/js/app.js
@@ -132,3 +132,64 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
+var scrubmax = 9;
+var scrub = 4;
+var percent = scrub / scrubmax;
+// Enter a speed between 0 and 180
+var level = 180 * percent;
+
+// Trig to calc meter point
+var degrees = 180 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data1 = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'speed',
+    text: level,
+    hoverinfo: 'text+name'},
+  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+  rotation: 90,
+  text: ['8-9', '6-8', '4-6', '2-4',
+            '1-2', '0-1', ''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                         'rgba(255, 255, 255, 0)']},
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout3 = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: 'Gauge Speed 0-100',
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge', data1, layout3);
